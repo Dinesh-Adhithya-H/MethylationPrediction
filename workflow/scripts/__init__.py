4c63936589596c5c94acf532f81d288ff52da1aa
abdeldar <abd.eldar1993@gmail.com>
2025-10-01 01:30:41 +0200

    some changes everywhere

--- workflow/scripts/__init__.py
+++ workflow/scripts/__init__.py
@@ -1,27 +1,44 @@
-import sklearn as sk
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
 import os
-
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression,LinearRegression
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.feature_selection import SelectKBest,chi2 
-from sklearn import svm 
-from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler,RobustScaler
-from sklearn.model_selection import train_test_split,KFold,GridSearchCV,cross_val_score,StratifiedKFold
-from sklearn.metrics import classification_report, accuracy_score, make_scorer,confusion_matrix
-from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
-from sklearn.neighbors import KNeighborsClassifier
-import pickle
-from sklearn.neural_network import MLPClassifier
-from sklearn.utils import shuffle
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import train_test_split,KFold,GridSearchCV,cross_val_score,StratifiedKFold
-from joblib import dump, load
 import sys
+import pickle
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn import svm
+from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    AdaBoostClassifier,
+    GradientBoostingClassifier
+)
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.preprocessing import (
+    MinMaxScaler,
+    StandardScaler,
+    MaxAbsScaler,
+    RobustScaler
+)
+from sklearn.model_selection import (
+    train_test_split,
+    KFold,
+    GridSearchCV,
+    cross_val_score,
+    StratifiedKFold
+)
+from sklearn.metrics import (
+    classification_report,
+    accuracy_score,
+    make_scorer,
+    confusion_matrix
+)
+from sklearn.utils import shuffle
+from joblib import dump, load
